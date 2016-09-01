a0d2faaa4d20da5bb34530aecfc6ecb2c555807c
Jelena <ajdacic.d.jelena@gmail.com>
2016-09-01 13:04:57 +0200

    signIn..

--- app/src/main/java/com/freelancewatermelon/factordiary/MainActivity.java
+++ app/src/main/java/com/freelancewatermelon/factordiary/MainActivity.java
@@ -1,13 +1,59 @@
 package com.freelancewatermelon.factordiary;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    public static final String ANONYMOUS = "anonymous";
+    private static final String TAG = "MainActivity";
+
+    private String mUsername;
+
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set default username is anonymous.
+        mUsername = ANONYMOUS;
+
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        if (mFirebaseUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(new Intent(this, SignInActivity.class));
+            finish();
+            return;
+        } else {
+            mUsername = mFirebaseUser.getDisplayName();
+            if (mFirebaseUser.getPhotoUrl() != null) {
+                Toast.makeText(this, mFirebaseUser.getPhotoUrl().toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d(TAG, "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
+
     }
 }
---
+++ app/src/main/java/com/freelancewatermelon/factordiary/SignInActivity.java
@@ -0,0 +1,7 @@
+package com.freelancewatermelon.factordiary;
+
+/**
+ * Created by mirna on 1.9.2016.
+ */
+public class SignInActivity {
+}
