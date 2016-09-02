8e387a9219281e288d64c09852a255216ca46abc
Milan <o0milanzoric0o@gmail.com>
2016-09-02 15:26:11 +0200

    SignIn page design

---
+++ app/src/main/java/com/freelancewatermelon/factordiary/Common/Utils.java
@@ -0,0 +1,24 @@
+package com.freelancewatermelon.factordiary.Common;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.Icon;
+import android.support.v4.content.ContextCompat;
+
+import com.freelancewatermelon.factordiary.R;
+
+import net.steamcrafted.materialiconlib.MaterialDrawableBuilder;
+
+/**
+ * Created by 1 on 9/2/2016.
+ */
+public class Utils {
+    public static Drawable getMaterialIconDrawable(Context context, MaterialDrawableBuilder.IconValue iconValue, int colorID){
+        Drawable drawable = MaterialDrawableBuilder.with(context) // provide a context
+                .setIcon(iconValue) // provide an icon
+                .setColor(ContextCompat.getColor(context, colorID)) // set the icon color
+                .setToActionbarSize() // set the icon size
+                .build(); // Finally call build
+        return drawable;
+    }
+}
--- app/src/main/java/com/freelancewatermelon/factordiary/SignInActivity.java
+++ app/src/main/java/com/freelancewatermelon/factordiary/SignInActivity.java
@@ -6,12 +6,15 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.freelancewatermelon.factordiary.Common.Utils;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -34,6 +37,7 @@ public class SignInActivity extends AppCompatActivity implements
     private static final int RC_SIGN_IN = 9001;
 
     private Button mGmailLoginButton;
+    private Toolbar toolbar;
 
 
     private GoogleApiClient mGoogleApiClient;
@@ -49,7 +53,8 @@ public class SignInActivity extends AppCompatActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         // TODO make action bar pretty with exit button and LoginBtn
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
+        initToolBar();
 
         //Initialize FirebaseAuth
         mFirebaseAuth = FirebaseAuth.getInstance();
@@ -58,13 +63,14 @@ public class SignInActivity extends AppCompatActivity implements
         // Assign fields
         mGmailLoginButton = (Button) findViewById(R.id.gmail_sign_in_button);
 
+        // Add button Left drawable (Google icon)
         // The method returns a MaterialDrawable, but as it is private to the builder you'll have to store it as a regular Drawable ;)
-        Drawable myDrawable = MaterialDrawableBuilder.with(this) // provide a context
-                .setIcon(MaterialDrawableBuilder.IconValue.WEATHER_RAINY) // provide an icon
-                .setColor(Color.WHITE) // set the icon color
+        Drawable myLeftBtnIcon = MaterialDrawableBuilder.with(this) // provide a context
+                .setIcon(MaterialDrawableBuilder.IconValue.GOOGLE) // provide an icon
+                .setColor(ContextCompat.getColor(this,R.color.colorBackground)) // set the icon color
                 .setToActionbarSize() // set the icon size
                 .build(); // Finally call build
-
+        mGmailLoginButton.setCompoundDrawablesWithIntrinsicBounds(myLeftBtnIcon, null, null, null );
 
         // Set click listeners
         mGmailLoginButton.setOnClickListener(this);
@@ -139,5 +145,22 @@ public class SignInActivity extends AppCompatActivity implements
                         }
                     }
                 });
+    }
+
+    public void initToolBar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setTitle(R.string.toolbarTitle);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(Utils.getMaterialIconDrawable(SignInActivity.this, MaterialDrawableBuilder.IconValue.CLOSE,R.color.colorTextWhite));
+        toolbar.setNavigationOnClickListener(
+        new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SignInActivity.this, "clicking the toolbar!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
     }
 }
